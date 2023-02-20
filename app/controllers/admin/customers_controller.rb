a40dae2985afe33e69da5d04909387bde1fb9972
EC2 Default User <ec2-user@ip-172-31-47-121.ap-northeast-1.compute.internal>
2023-02-20 13:49:52 +0000

    編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -24,14 +24,7 @@ before_action :authenticate_admin!
       render :edit
     end
   end
-
-  # def withdraw
-    # @customer = Customer.find(params[:format])
-    # @customer.update(is_deleted: true)
-    # flash[:notice] = "退会処理を実行いたしました"
-    # redirect_to admin_customers_path(@customer)
-  # end
-
+  
   def customer_params
     params.require(:customer).permit(:name, :introduction, :profile_image, :is_deleted)
   end
